331dc85380dbe8eab590e4fab398cf0c2bb8810f
unknown <reetesh105>
2022-07-30 23:45:17 +0530

    add my project

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./components/layout/Login";
+import SignUp from "./components/layout/Signup";
+import Compo from "./components/layout/Reetesh";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={"/compo"}>
+              On-Click
+            </Link>
+
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-in"}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-up"}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/compo" element={<Compo />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
+export default App;
---
+++ src/components/layout/Reetesh.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+export default class Login extends Component {
+  render() {
+    return (
+        <div className="rk">
+        <h3>    
+        <span class="odd">R</span>
+        <span class="even">E</span>
+        <span class="odd">E</span>
+        <span class="even">T</span>
+        <span class="odd">E</span>
+        <span class="even">S</span>
+        <span class="odd">H</span>
+        </h3>
+      </div>
+    );
+  }
+}
